956f2667bcfbc57fea45cff28f7391dab1886468
adonis386 <gonzalezadonis16@gmail.com>
2023-10-14 16:37:40 -0400

    Hack listo

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,8 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
-    return result  
+    txt = "fooziman"
+
+    change = txt.replace("o","0").replace("i","1").replace("a","@").upper()
+    split = list(change)
+    return split  
--- hack_9.py
+++ hack_9.py
@@ -3,6 +3,13 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 """
 
 def fn_hack_9():
-    result = [1,2,3]
-    #...
-    return result  
+    result = [1, 2, 3]
+    lista2 = []
+    index = 0
+    
+    while index < len(result):
+        lista2.append(result[index])
+        lista2.append("@")
+        index +=1
+    
+    return lista2
